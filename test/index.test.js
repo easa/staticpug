1bb9061e75bebe1db1d6dc0d3fcd1ef52517faf5
easa <eisanodehi@gmail.com>
2020-06-28 15:02:08 +0430

    remove lib/index

--- test/index.test.js
+++ test/index.test.js
@@ -5,3 +5,10 @@ test('Should return currect objects', () => {
   expect(typeof index['copyStatics']).toBe('function')
   expect(typeof index['renderHTMLs']).toBe('function')
 })
+
+const { copyStatics, renderHTMLs } = require('../index')
+
+test('Should return currect objects', () => {
+  expect(typeof copyStatics).toBe('function')
+  expect(typeof renderHTMLs).toBe('function')
+})
--- test/test-lib/index.test.js
+++
@@ -1,7 +0,0 @@
-const index = require('../../lib/index')
-
-test('Should return currect objects', () => {
-  expect(typeof index).toBe('object')
-  expect(typeof index['copyStatics']).toBe('function')
-  expect(typeof index['renderHTMLs']).toBe('function')
-})
