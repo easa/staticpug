a61d46442f4cc8da8dbdbbffbc86a0d07a32a5f3
easa <eisanodehi@gmail.com>
2020-06-28 14:54:00 +0430

    add jsdoc comments to index.js

--- index.js
+++ index.js
@@ -5,6 +5,18 @@ const {
 } = require('./lib')
 
 module.exports = {
+
+  /**
+   * Copy every file and nested directories (cp -R)
+   * @param {string} source The path to the thing to copy.
+   * @param {string} destination The path to the new copy.
+   */
   copyStatics,
+
+  /**
+   * 1.Reads HAML 2.Render template using 'pug' 3.Save HTML files  
+   * @param {string} SourcePagesPath The path to the template pages.
+   * @param {string} destination The path to the new HTML pages.
+   */
   renderHTMLs
 }
