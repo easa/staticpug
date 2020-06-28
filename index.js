52b85ec8d74ad9ad11c8617a3b964f763a9e77e4
easa <eisanodehi@gmail.com>
2020-06-28 15:06:58 +0430

    add jsdoc to index.js

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
+/**
+ * Copy every file and nested directories (cp -R)
+ * @param {string} source The path to the thing to copy.
+ * @param {string} destination The path to the new copy.
+*/
 module.exports.copyStatics = require('./lib/copyFileRecursive')
+
+/**
+ * 1.Reads HAML 2.Render template using 'pug' 3.Save HTML files  
+ * @param {string} SourcePagesPath The path to the template pages.
+ * @param {string} destination The path to the new HTML pages.
+ */
 module.exports.renderHTMLs = require('./lib/generateHTML')
